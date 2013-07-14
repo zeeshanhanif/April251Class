709165a232d2a2b0a862acab30de84f4492d68af
Zeeshan Hanif <zeeshanhanif@gmail.com>
2013-07-14 15:39:06 +0500

    Class work 14 July 2013

---
+++ 14_07_2013/DemoPhoneGap1/DemoPhoneGap1/bin/Debug/AppX/js/index.js
@@ -0,0 +1,28 @@
+﻿(function () {
+
+    function init() {
+        //alert("The device is ready!");
+        draw();
+    }
+
+    function draw() {
+        var svg = d3.select("body")
+                    .append("svg")
+                    .attr({
+                        width: "100%",
+                        height: "100%"
+                    });
+        svg.append("rect")
+            .attr({
+                x: 50,
+                y: 50,
+                width: 100,
+                height: 100,
+                fill: "red"
+            });
+    }
+
+    window.app = window.app ||  {};
+    app.init = init;
+    
+})();
